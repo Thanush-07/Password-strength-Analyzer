6339c346b116ab09e2620203c9b62b2d30d06f0c
Thanush-07 <thanushkumar722@gmail.com>
2025-06-24 01:03:19 +0530

    environment created

---
+++ passwordchecker/app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.urls import reverse
+# Create your views here.
+def index(request):
+    return render(request,'html files/index.html')
